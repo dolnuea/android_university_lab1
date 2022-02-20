e5b0998732bef210cf8c6bee5a4a554158b4c4fa
Luna Dagci <ldagci@albany.edu>
2022-02-20 00:34:12 -0500

    Update book layout with ranking, image, description and buy button

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -3,10 +3,13 @@ package com.codepath.bestsellerlistapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.BestSellerBooksRecyclerViewAdapter.BookViewHolder
 import com.codepath.bestsellerlistapp.R.id
 import com.codepath.bestsellerlistapp.R.layout
@@ -28,8 +31,15 @@ class BestSellerBooksRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         holder.mItem = books[position]
+        holder.mBookRanking.text = books[position].rank.toString()
+        Glide.with(holder.itemView)
+                .load(books[position].bookImageUrl)
+                .override(400, 400)
+                .into(holder.mBookImage)
         holder.mBookTitle.text = books[position].title
         holder.mBookAuthor.text = books[position].author
+        holder.mBookDescription.text = books[position].description
+//        holder.mBuyButton.text = books[position].amazonUrl
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
                 mListener?.onItemClick(book)
@@ -42,8 +52,12 @@ class BestSellerBooksRecyclerViewAdapter(
     }
 
     inner class BookViewHolder(val mView: View) : ViewHolder(mView) {
+        val mBookRanking: TextView = mView.findViewById(id.ranking) as TextView
+        val mBookImage: ImageView = mView.findViewById(id.book_image) as ImageView
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mBookDescription: TextView = mView.findViewById(id.book_description) as TextView
+        val mBuyButton: Button = mView.findViewById(id.buy_button) as Button
         var mItem: BestSellerBook? = null
 
         override fun toString(): String {
